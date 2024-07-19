0edff7dccf3f977187a2a2f8d3fdaf7acb3274b0
whgkswo <gjuk7238@gmail.com>
2024-07-19 17:54:32 +0900

    0.13.6.1 변수명 하나 변경

--- src/main/java/net/whgkswo/tesm/calendar/Time.java
+++ src/main/java/net/whgkswo/tesm/calendar/Time.java
@@ -12,28 +12,28 @@ public class Time {
         this.minute = (int) minute;
         this.second = (int) second;
     }
-    public String toString(TimeFormat format, boolean includeSecond){
+    public String toString(TimeFormat format, boolean secondIncluded){
         switch (format){
             case COLONAL_24H -> {
-                return includeSecond ? String.format("%d:%d:%d",hour,minute,second) :
+                return secondIncluded ? String.format("%d:%d:%d",hour,minute,second) :
                         String.format("%d:%d",hour,minute);
             }
             case COLONAL_12H -> {
                 String amPm = hour<=12 ? "AM" : "PM";
                 // 원칙적으로 정오는 오후 0시가 맞으나 예외적으로 오후 12시로 표기
                 int displayHour = (hour == 12) ? 12 : hour % 12;
-                return includeSecond ? String.format("%s %d:%d:%d",amPm,displayHour,minute,second) :
+                return secondIncluded ? String.format("%s %d:%d:%d",amPm,displayHour,minute,second) :
                         String.format("%s %d:%d",amPm,displayHour,minute);
             }
             case LINGUAL_24H -> {
-                return includeSecond ? String.format("%d시 %d분 %d초", hour,minute,second) :
+                return secondIncluded ? String.format("%d시 %d분 %d초", hour,minute,second) :
                         String.format("%d시 %d분", hour,minute);
             }
             case LINGUAL_12H -> {
                 String amPm = hour<12 ? "오전" : "오후";
                 // 원칙적으로 정오는 오후 0시가 맞으나 예외적으로 오후 12시로 표기
                 int displayHour = (hour == 12) ? 12 : hour % 12;
-                return includeSecond ? String.format("%s %d시 %d분 %d초", amPm,displayHour,minute,second) :
+                return secondIncluded ? String.format("%s %d시 %d분 %d초", amPm,displayHour,minute,second) :
                         String.format("%s %d시 %d분", amPm,displayHour,minute);
             }
             default -> {
